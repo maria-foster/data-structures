fe953f167b226a41d56acb1d47d110bd1a590db7
maria-foster <mariafoster@Marias-iMac.local>
2021-08-09 23:32:58 -0400

    implemented stack pop  with circularLinkedList

--- linkedList/circularLinked/node.go
+++ linkedList/circularLinked/node.go
@@ -2,11 +2,11 @@ package circularLinked
 
 //	the doubly circular linked list does not contain the NULL value in the previous field of the node.
 type Node struct {
-	previousNode *Node
-	nextNode *Node
-	value	string 		// could be any type
+	PreviousNode *Node
+	NextNode *Node
+	Value	string 		// could be any type
 }
 
 type CircularLinkedList struct {
-	firstNode *Node
+	FirstNode *Node
 }
--- stack/stack.go
+++ stack/stack.go
@@ -1,2 +1,18 @@
 package stack
 
+import (
+	lists "dataStructures/linkedList/circularLinked"
+)
+type Stack struct {
+	List	lists.CircularLinkedList
+}
+func (stack Stack) Pop() (*lists.Node) {
+	//  inserting and removing elements from the end of the list
+	// firstNode := stack.List.FirstNode
+	// poppedNode := stack.List.FirstNode.PreviousNode
+	// nextNode := stack.List.FirstNode.PreviousNode.NextNode
+	stack.List.FirstNode.PreviousNode = stack.List.FirstNode.PreviousNode.NextNode
+	stack.List.FirstNode.PreviousNode.NextNode.NextNode = stack.List.FirstNode
+	return stack.List.FirstNode.PreviousNode
+
+}
