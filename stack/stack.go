cbaac0bf63f3c0e78fdd93a70c179c75d5737f72
maria-foster <mariafoster@Marias-iMac.local>
2021-08-09 23:40:37 -0400

    Added push, pop, peek, is empty for stack

--- stack/stack.go
+++ stack/stack.go
@@ -6,6 +6,8 @@ import (
 type Stack struct {
 	List	lists.CircularLinkedList
 }
+// FOUR FUNCTHIONS: pop, push, peek, isEmpty
+
 func (stack Stack) Pop() (*lists.Node) {
 	//  inserting and removing elements from the end of the list
 	// firstNode := stack.List.FirstNode
@@ -15,4 +17,26 @@ func (stack Stack) Pop() (*lists.Node) {
 	stack.List.FirstNode.PreviousNode.NextNode.NextNode = stack.List.FirstNode
 	return stack.List.FirstNode.PreviousNode
 
+}
+
+func (stack Stack) Push( newNode *lists.Node) (*lists.Node) {
+	//  inserting and removing elements from the end of the list
+	// firstNode := stack.List.FirstNode
+	// pushedNode := newNode
+	// nextNode := stack.List.FirstNode.PreviousNode.NextNode
+	newNode.NextNode = stack.List.FirstNode
+	newNode.PreviousNode =  stack.List.FirstNode.PreviousNode.NextNode
+	stack.List.FirstNode.PreviousNode = newNode
+	stack.List.FirstNode.PreviousNode.NextNode = newNode
+	return stack.List.FirstNode.PreviousNode
+
+}
+
+func (stack Stack) IsEmpty() (bool){
+	return stack.List.FirstNode == nil
+}
+
+func (stack Stack) Peek() (*lists.Node) {
+	// returns the node at the top of the stack
+	return stack.List.FirstNode.PreviousNode
 }
